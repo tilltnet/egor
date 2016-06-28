aae6b87cea591305f50a660d4b62c22e1f6edc9e
tilltnet <github@tillt.net>
2016-06-28 22:34:51 +0200

    Added directed option and fixed weight option. Added tests for density.

--- R/density.R
+++ R/density.R
@@ -9,18 +9,31 @@
 #' functions.
 #' @param max.netsize Optional parameter. Only needed if the density information
 #' was only collected for a subsample of all alteri per network.
+#' @param directed logical indicating if the alter-alter relation data/ edges 
+#' are directed or undirected.
 #' @return returns a \code{vector} of network density values.
 #' @keywords ego-centric network analysis
 #' @export
-egoR.density <- function(egoR, weight = "weight", max.netsize = NULL) {
-  dyaden_real <- plyr::ldply(egoR[["edges"]], .fun = function(x) sum(x[[weight]], na.rm = T))[[2]]
-  netsize <- egoR$results$netsize
+egoR.density <- function(egoR, weight = NULL, max.netsize = NULL, directed = FALSE) {
+
+  if(!is.null(weight)) {
+    dyaden_real <- plyr::ldply(egoR[["edges"]], .fun = function(x) sum(x[[weight]], na.rm = T))[[2]]
+    
+  } else {
+    dyaden_real <- plyr::ldply(egoR[["edges"]], .fun = function(x) NROW(x))[[2]]
+  }
   
+  netsize <- egoR$results$netsize
   if(!is.null(max.netsize)) {
     netsize[netsize > max.netsize] <- max.netsize
   }
-    
-  dyad_poss <- (netsize^2-netsize)/2
+  
+  dyad_poss <- (netsize^2-netsize)
+
+  if(!directed) {
+    dyad_poss <- dyad_poss/2
+  }
+
   density <- as.vector(dyaden_real / dyad_poss)
   density
 }
