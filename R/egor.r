dc4a1046bbccf16b488a983093b9878aaf7036f2
tilltnet <github@tillt.net>
2017-05-14 21:57:27 +0200

    Introduced egor() and summary.egor()

---
+++ R/egor.r
@@ -0,0 +1,86 @@
+#' egor - a data class for ego-centered network data.
+#'
+#' The function \code{egor()} is used to create an egor object from 
+#' ego-centered network data.
+#' @param alters.df \code{data.frame} containing the alters.
+#' @param egos.df \code{data.frame} containing the egos.
+#' @param alter_ties.df \code{data.frame} containing the alter-alter relations in
+#' the style of and edge list.
+#' @param egoID Name of ego ID variable.
+#' @param max.netsize Optional parameter. Constant value used if the
+#' number of alteri whose relations were collected is limited.
+#' @return returns an \code{egor} object. An egor is a \code{data.frame}, which
+#' consists of an ego ID column, nested columns for alter and alte-alter tie 
+#' data and regular columns for ego-level data.
+#' @details The parameters alters.df, egos.df and alter_ties need to share a common
+#' ego ID variable, with corresponding values. Of the three parameters only alters.df
+#' is necessary to create an egor object, egos.df and alter_ties.df are optional.
+#' @keywords ego-centric network analysis
+#' @examples 
+#' 
+#' @export
+egor <- function(alters.df, egos.df = NULL, alter_ties.df = NULL, egoID="egoID") {
+  # FUN: Inject empty data.frames with correct colums in to NULL cells in 
+  # $alters and $alter_ties
+  inj_zero_dfs <- function(x, y) {
+    null_entries <- sapply(x[[y]], is.null)
+    zero_df <- x[[y]][!null_entries][[1]][0, ]
+    zero_dfs <- lapply(x[[y]][null_entries], FUN = function(x) zero_df)
+    x[null_entries, ][[y]] <- zero_dfs
+    x
+  }
+  
+  # Create initial egor object from alters
+  egor <- tidyr::nest_(data = alters.df, 
+                             key_col = "alters", 
+                             names(alters.df)[names(alters.df) != egoID]) # Select all but the egoID column for nesting
+  
+  # If speciefied add ego data to egor
+  if(!is.null(egos.df)) {
+    alter_ties.tib <- tidyr::nest_(data = alter_ties.df, 
+                                   key_col = "alter_ties",    
+                                   names(alter_ties.df)[names(alter_ties.df) != egoID])
+    egor <- dplyr::full_join(egor, alter_ties.tib, by = egoID)
+    egor <- inj_zero_dfs(egor, "alters")
+  }
+  
+  # If specified add alter_ties data to egor
+  if(!is.null(alter_ties.df)) {
+    egor <- dplyr::full_join(egor, egos.df, by = egoID)
+    egor <- inj_zero_dfs(egor, "alter_ties")
+    
+  }
+  
+  # Check If egoIDs valid
+  if (length(unique(egor[[egoID]])) < length(egor[[egoID]]))
+    warning(paste(egoID, "values are note unique. Check your 'egos.df' data."))
+  
+  # Add meta attribute
+  #  ----><----
+
+  class(egor) <- c("egor", class(egor))
+  egor
+}
+
+filter_egor <- function(egor, obj = c("alters", "alter_ties"), cond) {
+  
+}
+
+
+summary.egor <- function(egor) {
+  # Network count
+  nc <- NROW(egor)
+  
+  # Average netsize
+  nts <- sum(unlist(lapply(egor$alters, FUN = NROW))) / nc
+  
+  # Average netsize
+  if("alter_ties" %in% names(egor)) dens <- sum(egoR.density(egor)) / nc else dens <- NULL
+  
+  cat(paste(nc, "Egos/ Ego Networks", "\nAverage Netsize", nts, "\n"))
+  if(!is.null(dens)) cat(paste("Average Density", dens))
+
+  # Meta Data
+
+}
+
---
+++ tests/testthat/test_egor.r
@@ -0,0 +1,95 @@
+asd <- egor:::generate.sample.ego.data(32, 20)
+alters.df <- asd$alteri
+egos.df <-  asd$egos
+alters.df$egoID <- as.integer(alters.df$egoID)
+alter_ties <- mapply(FUN = function(x, y) data.frame(egoID = y, x), asd$edges, 1:length(asd$edges), SIMPLIFY = F)
+
+alter_ties.df <- do.call(rbind, alter_ties)
+
+
+
+
+library(tidyr)
+library(dplyr)
+
+
+class(alters.df$egoID)
+class(egos.df$egoID)
+class(alter_ties.df$egoID)
+
+
+e1 <- egor(alters.df, egos.df, alter_ties.df)
+summary(e1)
+ego_density(e1)
+ego_density(e1, weight = "weight")
+
+ego_density(e1$alter_ties, e1$alters, weight = "weight")
+ego_density(alter_ties.df, alters.df, weight = "weight")
+
+
+NROW(e1$alter_ties[[1]])
+NROW(e1$alters[[1]])
+x <- e1$alter_ties
+weight = "weight"
+
+
+
+err_d <- generate.sample.ego.data(32, 20)
+
+
+# Checking for egos without alters
+alters <- unnest(err_d[1:2])
+alter_ties <- unnest(err_d[c(1,3)])
+alters <- alters[!alters$egoID %in% c(1,2,3), ]
+
+err_d1 <- egor(alters, err_d[c(-2, -3)], alter_ties)
+View(err_d1)
+
+View(unnest(err_d1[1:2]))
+
+
+# Checking egos without alter_ties
+alter_ties <- alter_ties[!alter_ties$egoID %in% c(4,5,9), ]
+err_d2 <- egor(alters, err_d[c(-2, -3)], alter_ties)
+
+
+# Checking for alters and alter_ties without egos
+egos <- err_d[c(-2, -3)]
+egos <- egos[sample(egos$egoID, 24), ]
+err_d3 <- egor(alters, egos, alter_ties)
+
+# Non-Unique egoID in ego data
+egos <- err_d[c(-2, -3)]
+egos <- rbind(egos, cbind(egos[1:4, 1], egos[5:8, -1] ))
+err_d4 <- egor(alters, egos, alter_ties)
+
+summary(err_d4)
+ego_density(err_d4)
+
+
+# filter
+cond_1 <- "alter.sex"
+cond_2 <- "w"
+e1$alters[[1]][cond ==, ]
+lapply(X= e1$alters, FUN = function(x, att, cond) 
+  x[x[att] == cond, ]
+, att = "alter.sex", cond = "w")
+
+
+dplyr::filter(e1$alters[[1]], alter.sex == "w")
+
+
+filter()
+
+if
+
+dplyr::filter_
+
+class(~13<2)
+lm
+
+
+cond <- ~alter.sex == "w"
+if(eval(cond, parent.frame())) {}
+cond
+model.matrix.default
