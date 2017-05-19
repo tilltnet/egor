0fa37ea9058a9c404f82588e3dc4c2d6bb5a30a4
Pavel N. Krivitsky <pavel@uow.edu.au>
2017-05-19 02:16:58 -0700

    subset.egor() and [.egor() probably work now.

--- R/subset.egor.R
+++ R/subset.egor.R
@@ -97,7 +97,12 @@ subset.egor <- function(x, subset, aspect = c("egos","alters","ties"), ...){
 
   switch(aspect,
          egos = {
-           x[i,j,drop=FALSE]
+           # Subset using the tibble's method, then copy over all
+           # attributes except for the ones that could have changed.
+           xt <- getS3method("[", "tbl_df")(x,i,j,drop=FALSE)
+           for(a in setdiff(names(attributes(x)), c("names", "rownames")))
+             attr(xt, a) <- attr(x, a)
+           xt
          },
          alters = {
            x$.alters <- lapply(seq_len(nrow(x)), function(k){
