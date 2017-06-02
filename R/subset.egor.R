8ceca92097ebad1cdbec53aa90b119008e998778
Pavel N. Krivitsky <pavel@uow.edu.au>
2017-06-01 22:55:00 -0700

    [.egor, now correctly selects elements of .alters and .aaties when selecting columns. Partial fix for #10.

--- R/subset.egor.R
+++ R/subset.egor.R
@@ -182,8 +182,8 @@ subset.egor <- function(x, subset, aspect = c("egos","alters","ties"), ...){
            if(is.list(i)) i <- unlist(i)
            bracket <- getS3method("[", "tbl_df")
            xt <- bracket(x,i,j,drop=FALSE)
-           if(! ".alters"%in%names(xt)) xt <- cbind(xt, .alters=bracket(x,,".alters"))
-           if(".aaties"%in%names(x) && ! ".aaties"%in%names(xt)) xt <- cbind(xt, .aaties=bracket(x,,".aaties"))
+           if(! ".alters"%in%names(xt)) xt <- cbind(xt, .alters=bracket(x,i,".alters"))
+           if(".aaties"%in%names(x) && ! ".aaties"%in%names(xt)) xt <- cbind(xt, .aaties=bracket(x,i,".aaties"))
            for(a in setdiff(names(attributes(x)), c("ego.design", "names", "row.names")))
              attr(xt, a) <- attr(x, a)
            attr(xt, "ego.design") <- attr(x, "ego.design")[i,]
