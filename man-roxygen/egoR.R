a8f865964d56039af1eb6afab8fa4dde1ac5c0cc
tilltnet <github@tillt.net>
2017-08-04 12:44:02 +0200

    Deletions of obsolete documentation.

--- man-roxygen/egoR.R
+++
@@ -1,6 +0,0 @@
-#' @param A \code{list} of six objects - the **egoR** object: (1) egos.df: \code{dataframe} of all
-#' egos and their attributes; (2) alters.df: \code{dataframe} of all alters;
-#' (3) alters.list: \code{list} of \code{dataframes} of all alters per ego;
-#' (4) edges: \code{list} of \code{dataframes} with edge lists per network; (5)
-#' graphs: \code{list} of igraph objects; (6) results: a result \code{dataframe},
-#' pre-populated with the network size of each network
