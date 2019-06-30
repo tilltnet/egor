d688a2d7bc233157f17922ad03c171e2ca4cbc4f
tilltnet <github@tillt.net>
2019-06-30 19:15:13 -0400

    started activate documentation

--- R/activation.R
+++ R/activation.R
@@ -1,3 +1,11 @@
+#' Activate ego, alter or alter-alter tie data llevel of an egor object
+#'
+#' This function activates one of the data levels of an egor object, so that the dplyr verbs know which level to execute on.
+#' @param obj The \code{egor} object.
+#' @param what \code{Character} naming the level to activate, this can be "ego", "alter" or "aatie".
+#' @keywords ego-centered network
+#'
+#' @export
 activate <- function(obj, what) {
   what <- match.arg(what, UNITS)
   attr(obj, "active") <- what
