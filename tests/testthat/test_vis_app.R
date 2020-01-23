da4c498106edd69411c4b7e01e2c9cab02f3b764
tilltnet <github@tillt.net>
2020-01-23 11:50:16 -0500

    fix: resolved reactive issues

--- tests/testthat/test_vis_app.R
+++ tests/testthat/test_vis_app.R
@@ -1,10 +1,40 @@
 context("test_vis_app.R")
 
 if (FALSE) {
-  library(shiny)
+  library(purrr)
   library(egor)
   f <- make_egor(50, 50)
-  ab <- filter(e, sex == "w")
+  ab <- filter(f, sex == "w")
+  data("allbus_2010_simulated")
+  raw_data <- allbus_2010_simulated
+  
+  ## -----------------------------------------------------------------------------
+  var_labels <- map_chr(raw_data, ~attr(., "label"))
+  
+  var_labels <- gsub("[,\\.:;><?+()-]", " ", var_labels)
+  var_labels <- gsub("\\s+", "_", trimws(var_labels))
+  
+  ## -----------------------------------------------------------------------------
+  var_labels <- gsub("FREUND_IN_._", "", var_labels)
+  var_labels <- gsub("^KONTAKT_._", "", var_labels)
+  
+  ## -----------------------------------------------------------------------------
+  names(raw_data) <- make.unique(var_labels, sep = "")
+  
+  ## -----------------------------------------------------------------------------
+  split_freunde <- 
+    raw_data %>% 
+    filter(FRAGEBOGENSPLIT_F020 == 1)
+  
+  ## -----------------------------------------------------------------------------
+  e_freunde <- onefile_to_egor(
+    egos = split_freunde,
+    ID.vars = list(ego = "IDENTIFIKATIONSNUMMER_DES_BEFRAGTEN"),
+    netsize = split_freunde$ANZ_GENANNTER_NETZWERKPERS_SPLIT_1,
+    attr.start.col = "GESCHLECHT",
+    attr.end.col = "SPANNUNGEN_KONFLIKTE2",
+    aa.first.var = "KENNEN_SICH_A_B",
+    max.alters = 3)
+  
   egor_vis_app()
 }
-
