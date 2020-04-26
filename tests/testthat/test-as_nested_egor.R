eb21c938fdf219a65641893cdbe96b2ae0b58023
tilltnet <github@tillt.net>
2020-04-26 16:12:41 -0400

    fix: now depending on R 3.5.0 in order to be able to include data as .rda files

---
+++ tests/testthat/test-as_nested_egor.R
@@ -0,0 +1,32 @@
+test_that("as_nested_egor works", {
+  as_nested_egor(make_egor(5,5))
+})
+
+test_that("as_nested_egor can be reversed to egor", {
+  old_egor_obj <- as_nested_egor(make_egor(5,5))
+  old_egor_obj <- old_egor_obj %>% 
+    mutate_at(vars(.alts, .aaties), ~purrr::map(., function(x) select(x, -.egoID)))
+    
+  alts <- old_egor_obj %>%
+    select(.egoID, .alts) %>%
+    tidyr::unnest(.alts)
+  
+  aaties <- old_egor_obj %>%
+    select(.egoID, .aaties) %>%
+    tidyr::unnest(.aaties)
+  
+  egos <- old_egor_obj %>%
+    select(-.alts, -.aaties)
+  
+  egor(
+    alts,
+    egos,
+    aaties,
+    ID.vars = list(
+      ego = ".egoID",
+      alter = ".altID",
+      source = ".srcID",
+      target = ".tgtID"
+    )
+  )
+})
