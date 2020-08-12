013553f927dd713be865c153bd3ac266a3defb5d
tilltnet <github@tillt.net>
2020-08-12 18:01:00 -0400

    fix: ungrouping results, to be on the safe side, #44

--- R/composition.R
+++ R/composition.R
@@ -39,7 +39,7 @@ composition <- function(object, alt.attr, absolute = FALSE) {
       select(.egoID, tmp, prop) %>%
       tidyr::spread(tmp, prop)
   }
-  comp(group_by(object$alter, .egoID))
+  ungroup(comp(group_by(object$alter, .egoID)))
 }
 
 
