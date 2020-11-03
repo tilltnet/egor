de540cc2de683d152aad06e32d3f8b753aac6424
tilltnet <github@tillt.net>
2020-11-03 12:03:06 -0500

    feat: added egor_options() and set values for  global options defaults #3

--- R/zzz.R
+++ R/zzz.R
@@ -15,3 +15,40 @@ RESERVED_COLNAMES <- c(".egoRow",
                        ".srcRow", 
                        ".tgtRow", 
                        unlist(IDVARS))
+
+egor_default_options <- 
+  list(
+    egor.return.results.with.design = TRUE,
+    egor.print.rows.active.level = 3,
+    egor.print.rows.inactive.level = 3,
+    egor.print.switch.active.level.to.top = TRUE
+  )
+
+.onLoad <- function(libname, pkgname) {
+  op <- options()
+  toset <- !names(egor_default_options) %in% names(op)
+  options(egor_default_options[toset])
+  
+  invisible()
+}
+
+#' Display names and values of global egor options.
+#' @details Currently egor makes use of the following global options. Use
+#' `options()` to change values.
+#' ## egor.print.rows.active.level
+#' - `Numeric`. Amount of rows to display when printing the active level of an `egor` object.
+#' ## egor.print.rows.inactive.level
+#' - `Numeric`. Amount of rows to display when printing the inactive levels of an `egor` object.
+#' ## egor.print.switch.active.level.to.top
+#' - `Logical`. When printing an egor object, should the active data-level always 
+#' be printed first?
+#' ## egor.return.results.with.design
+#' - `Logical`. `egor` functions that return ego-level results (e.g. one value per
+#'  ego) return a `svy_tbl` object containing the `ego_design()`, when this is set to `TRUE`.
+#' @export
+egor_options <-
+  function() {
+    op <- options()
+    op[names(egor_default_options)]
+  }
+
