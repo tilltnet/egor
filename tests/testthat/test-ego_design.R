ec54c96e45bf4eb843a905e9b705a238ed56ec3f
Pavel N. Krivitsky <p.krivitsky@unsw.edu.au>
2024-10-02 23:42:56 +1000

    docs: ego_design (both in egor and as LHS) expanded and suggests alist() rather than list(), with examples added; hint provided on error
    fixes #90

--- tests/testthat/test-ego_design.R
+++ tests/testthat/test-ego_design.R
@@ -26,6 +26,10 @@ test_that("ego_density works with ego_design", {
   ego_design(x) <- list(weight = "sampling_weight")
   library(srvyr)
   expect_error(ego_density(x), NA)
+
+  ## tidyselect arguments work as well
+  ego_design(x) <- alist(weight = sampling_weight)
+  expect_error(ego_density(x), NA)
 })
 
 test_that("survey_mean and svymean work with ego_design", {
@@ -40,6 +44,17 @@ test_that("survey_mean and svymean work with ego_design", {
     srvyr::summarise(mean_dens = srvyr::survey_mean(density)), NA)
   
   expect_error(survey::svymean(~density, ego_density(x)), NA)
-
-  
 })
+
+o <- options(useFancyQuotes = FALSE, width = 999)
+
+test_that("sensible error message is produced when using list() instead of alist()", {
+  expect_error(
+    egor(alters32, egos32, aaties32,
+         ID.vars = list(ego = ".EGOID", alter = ".ALTID", source = ".SRCID", target =  ".TGTID"),
+         ego_design = list(strata = sex)),
+    ".* Did you pass ego design variable names unquoted and wrap them in 'list\\(\\)' rather than 'alist\\(\\)'\\?.*"
+  )
+})
+
+options(o)
